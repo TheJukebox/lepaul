38ddf3dec3c2582ac6ed94e66293e41644516dbf
Joshua Grimshaw <josheewa@gmail.com>
2017-08-08 09:28:15 +1000

    Added comments
    Added some more comments for clarity and changed the albumDL function to imgurDL, in case I decide to add more sites or something.

--- lepaul.py
+++ lepaul.py
@@ -7,15 +7,18 @@ import os
 
 urlError="[*] Invalid URL! You need to point lepaul to a valid imgur album URL."
 
-def albumDL(albumURL):
-    """Downloads the album."""
+def imgurDL(albumURL):
+    """Downloads imgur albums."""
     
     global urlError
     imgNum=0
-
+    
+    # make sure we can handle an exception from a bad URL or input.
     try:
         req=urllib.request.Request(albumURL)
         res=urllib.request.urlopen(req)
+        # finally we get to scrape and
+        # extract what we need
         try:
             req
             albumData=BeautifulSoup(res,'html.parser')
@@ -28,6 +31,8 @@ def albumDL(albumURL):
                     getURL=image.get('src')
                     imgURL="//i.imgur.com/"
                     imgNum=imgNum+1
+                    # make sure we're only grabbing the images
+                    # we want and not ads or buttons on the page.
                     if imgURL in getURL:
                         newURL=str("http:"+getURL)
                         fileName=str('image-{}.png').format(imgNum)
@@ -61,14 +66,17 @@ def main():
     usage examples:
         lepaul.py -u "http://www.imgur.com/a/album"
     """
-
+    
+    # check over the args and make sure everything is in order.
     if len(sys.argv) >= 2: 
         if "-h" in sys.argv:
             print(usageHelp)
         elif sys.argv[1] == "-u":
+            # check the user input make sure we're
+            # getting an imgur album
             try:
                 if str("imgur.com/a/") in sys.argv[2]:
-                    albumDL(sys.argv[2])
+                    imgurDL(sys.argv[2])
                 else:
                     print(urlError)
             except IndexError:
