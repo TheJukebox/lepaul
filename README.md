7569fdce6d8bce4011ac6926f1ddca02c944f3e6
Joshua Grimshaw <josheewa@gmail.com>
2017-08-23 19:47:30 +1000

    Added new commands
    Added the chunkmode and name commands and updated the readme to reflect the changes.

--- README.md
+++ README.md
@@ -4,25 +4,38 @@ lepaul
 
 lepaul is a program that fetches images from imgur album links and downloads them into a convenient folder wherever you run the script.
 
-**Usage:**
-    
-    lepaul.py -u "http://imgur.com/a/tI0m7"
-   lepaul will run, fetch the images, and create a folder wherever lepaul.py is located.
-   The folder will have the same name as the album on imgur. Each image in the album will be numbered
-   according to its location in the album, so beware of albums that are backwards or otherwise out of order if
-   you like to keep your images organised.
-   
-    lepaul.py -h
-   This will display all the help you need (if you really need it) to run lepaul.
+### **Usage:**
+
+To run lepaul with default settings simply use the **URL** command:
+	
+	lepaul.py -u "http://imgur.com/a/tI0m7"
+
+By default, albums will have the name they've been given on Imgur. You can change this with the **name** command:
+	
+	lepaul.py -u "http://imgur.com/a/tI0m7" -n "Name"
+	 
+If you only want certain images from an album, you can use **chunkmode** to grab a slice of it:
+
+	lepaul.py -u "http://imgur.com/a/tI0m7" -c
+	
+lepaul will detect you've activated chunkmode and request integers once it's scraped the data from the Imgur page. This slice includes images at the start and end of the range provided.
+
+You can use all these commands in combination:
+
+	lepaul.py -u "http://imgur.com/a/tI0m7" -c -n "Album"
+	
+Finally, if you need help you can use the **help** command:
+
+	lepaul.py -h
 
 ## Requirements ##
 
 #### *Python 3* ####
-https://www.python.org/downloads/
+<https://www.python.org/downloads/>
 
 #### *Beautiful Soup 4* ####
-https://www.crummy.com/software/BeautifulSoup/
+<https://www.crummy.com/software/BeautifulSoup/>
 
 **Installing BS4 using pip:**
 
-    pip install beautifulsoup4
+	pip install beautifulsoup4
--- lepaul.py
+++ lepaul.py
@@ -5,24 +5,41 @@ import urllib
 import sys
 import os
 
-urlError="[*] Invalid URL! You need to point lepaul to a valid imgur album URL."
-
-def imgurWrite(images):
+def imgurWrite(images,albumTitle):
     """Writes the data collected in the imgurParse function."""
 
     imgNum=1
-    for i in images:
-        fileName=str("image-{}.png").format(imgNum)    
-        imgNum=imgNum+1
-        f=open(fileName,'wb')
-        f.write(urllib.request.urlopen(i).read())
-        f.close()
-        print("#",i,"has been successfully downloaded.")
 
-def imgurParse(albumURL):
+    print(len(images),"images found.") 
+    # try to create a directory based on the album's title
+    try:
+        os.mkdir(albumTitle)
+        print("Folder \""+albumTitle+"\" created.")
+        os.chdir(albumTitle)
+        for i in images:
+            fileName=str("image-{}.png").format(imgNum)    
+            imgNum=imgNum+1
+            f=open(fileName,'wb')
+            f.write(urllib.request.urlopen(i).read())
+            f.close()
+            print("#",i,"has been successfully downloaded.")
+    except FileExistsError:
+        print("[*] You already have an album or folder by this name!")
+
+def imgurChunk(images,albumTitle,chunkStart,chunkEnd):
+    """Grabs a chunk of the album, as specified in the main function."""
+
+    imgNum=1
+
+    print(len(images),"images found.")
+    print("Cutting out the chunk...")
+    chunk = images[chunkStart:chunkEnd]
+    print("Chunk contains",len(chunk),"images.")
+    imgurWrite(chunk,albumTitle)
+
+def imgurParse(albumURL,chunkMode):
     """Parses the html for use in the imgurWrite function."""
 
-    global urlError
     # there's probably a better name for this list
     images=[]
     
@@ -31,36 +48,39 @@ def imgurParse(albumURL):
         res=urllib.request.urlopen(req)
         # here we scrape and extract
         # whatever we need
-        try:
-            req
-            albumData=BeautifulSoup(res,'html.parser')
+        req
+        albumData=BeautifulSoup(res,'html.parser')
+        if "-n" in sys.argv:
+            nLocate=sys.argv.index('-n')
+            albumTitle=str(sys.argv[nLocate+1])
+        else:
             albumTitle=albumData.title.string[:-17].lstrip()
-            # try to create a directory based on the album's title
-            try:
-                os.mkdir(albumTitle)
-                print("Folder \""+albumTitle+"\" created.")
-                os.chdir(albumTitle)
-            except FileExistsError:
-                print("[*] You already have an album or folder by this name!")
-            # this for loop gathers the images and compiles them into
-            # a list called images if they're what we're looking for
-            for img in albumData.find_all('img'):
-                getURL=img.get('src')
-                imgURL="//i.imgur.com/"
-                if imgURL in getURL:
-                    newURL=str("http:"+getURL)
-                    images.append(newURL)
-            imgurWrite(images)
-        except urllib.error.HTTPError:
-            print(urlError) 
-    except (urllib.error.URLError,ValueError):
-        print(urlError) 
+        # this for loop gathers the images and compiles them into
+        # a list called images if they're what we're looking for
+        for img in albumData.find_all('img'):
+            getURL=img.get('src')
+            imgURL="//i.imgur.com/"
+            if imgURL in getURL:
+                newURL=str("http:"+getURL)
+                images.append(newURL)
+        if chunkMode == True:
+            print("Chunk mode is active.")
+            chunkStart=int(input("<#lepaul>Chunk Start: "))
+            chunkEnd=int(input("<#lepaul>Chunk End: "))
+            print("[*] lepaul requires an integer for chunk mode.")
+            imgurChunk(images,albumTitle,chunkStart-1,chunkEnd)
+        else:
+                imgurWrite(images,albumTitle)
+    except (urllib.error.URLError,urllib.error.HTTPError):
+        print("[*] Invalid URL: lepaul needs an imgur album URL to work!") 
+    except ValueError:
+        print("[*] Invalid Input: chunkmode needs to be provided with integers!")
 
 def main():
     """Handles user input and prints help/usage info."""
     
-    global urlError
-    usage="lepaul.py -u \"http://www.imgur.com/a/babpls\" -h --help"
+    chunkMode = False
+    usage="lepaul.py -u \"http://www.imgur.com/a/babpls\" -h --help -c --chunkmode -n --name"
     usageHelp="""
     lepaul is a python script for grabbing images from an imgur album.
     Simply provide it with an imgur album url and it'll do the rest for you!
@@ -69,10 +89,15 @@ def main():
     Commands
     ========
     -u      allows you to input a url.
+    -c      activates chunkmode.
+    -n      allows you to set the name of an album folder.
     -h      displays this help text.
 
     usage examples:
         lepaul.py -u "http://www.imgur.com/a/album"
+        lepaul.py -u "http://www.imgur.com/a/album" -c
+        lepaul.py -u "http://www.imgur.com/a/album" -n "New Album"
+        lepaul.py -u "http://www.imgur.com/a/album" -c -n "New Album"
     """
     
     # check over the args and make sure everything is in order.
@@ -83,12 +108,16 @@ def main():
             # check the user input make sure we're
             # getting an imgur album
             try:
-                if str("imgur.com/a/") in sys.argv[2]:
-                    imgurParse(sys.argv[2])
+                if "-c" in sys.argv:
+                    chunkMode = True
                 else:
-                    print(urlError)
+                    chunkMode = False
+                if str("imgur.com/a/") in sys.argv[2]:
+                    imgurParse(sys.argv[2],chunkMode)
+                else:
+                    print("[*] Invalid URL: lepaul needs an imgur album URL to work!")
             except IndexError:
-                print("Invalid number of arguments."
+                print("[*] Invalid Input: lepaul requires more arguments!"
                         "\n"+usage)
     else:
         print(usage)
